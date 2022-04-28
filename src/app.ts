da4b00d1a3b5b1c00a94a0239cb1fcedb232d98a
Francisco <fjcalderon@gmail.com>
2022-04-28 15:15:35 -0300

    Add endpoints to handle consignments

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import path from "path";
+
 
 import { loadApiEndpoints } from "./controllers/api";
 
--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,7 +1,50 @@
 import { Application, Request, Response } from "express";
+import multer from "multer";
+import Datastore from "nedb-promises";
+import fs from "fs";
+
+const FILE_PATH = './files/';
+const upload = multer({dest: FILE_PATH });
+interface Consignment {
+  _id?: string;
+  filename: string;
+  blindedutxo: string;
+}
+
+let ds = Datastore.create();
 
 export const loadApiEndpoints = (app: Application): void => {
-  app.get("/api", (req: Request, res: Response) => {
-    return res.status(200).send({hello: "Hi!"});
+  app.get("/consignment", async (req: Request, res: Response) => {
+    try {
+      if (!!req.query.blindedutxo) {
+        const c: Consignment | null = await ds.findOne({ blindedutxo: req.query.blindedutxo });
+        if (!c) {
+          return res.status(500).send({ success: false });
+        }
+        const file_buffer  = fs.readFileSync(FILE_PATH + c.filename);
+
+        return res.status(200).send({
+          success: true,
+          consignment: file_buffer.toString('base64'),
+        });
+      }
+
+      res.status(500).send({ success: false });
+    } catch (error) {
+      res.status(500).send({ success: false });
+    }
+  });
+
+  app.post("/consignment", upload.single('consignment'), async (req: Request, res: Response) => {
+    try {
+      const consignment: Consignment = {
+        filename: req.file?.filename || '',
+        blindedutxo: req.body.blindedutxo,
+      };
+      await ds.insert(consignment);
+      return res.status(200).send({ success: true });
+    } catch (error) {
+      res.status(500).send({ success: false });
+    }
   });
 };
